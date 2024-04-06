6402afceeeab3f7766e7767a6bde16fa561f3b31
omaks45 <danielgozie5@gmail.com>
2024-04-06 19:57:32 +0100

    edited

--- src/modules/authentication/individualUserAuth/individualAuth.route.ts
+++ src/modules/authentication/individualUserAuth/individualAuth.route.ts
@@ -1,9 +1,1 @@
-import { Router } from "express";
-import { individualUserRegistration } from "../individualUserAuth/individualUserAuth.controller";
 
-const router = Router();
-
-// routes
-router.route("/register").post(individualUserRegistration);
-
-export default router;
