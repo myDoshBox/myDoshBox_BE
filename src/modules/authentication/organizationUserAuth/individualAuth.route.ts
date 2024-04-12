90eb48f66664f86638b5b37724d01fea5f14fcd2
omaks45 <danielgozie5@gmail.com>
2024-04-12 16:45:01 +0100

    more unwanted codes removed

--- src/modules/authentication/organizationUserAuth/individualAuth.route.ts
+++
@@ -1,8 +0,0 @@
-import Router from "express";
-import { organizationRegister } from "./individualUserAuth.controller";
-
-const router = Router();
-
-router.post("/register", organizationRegister)
-
-export default router; 
