b132353e3799c93e52da51c7bff912fd53b21088
Ajayi favour <97252800+fevico@users.noreply.github.com>
2024-04-12 07:21:35 +0100

    Update individualUserAuth.controller.ts

--- src/modules/authentication/organizationUserAuth/individualUserAuth.controller.ts
+++ src/modules/authentication/organizationUserAuth/individualUserAuth.controller.ts
@@ -1,15 +1,1 @@
-import oranizationUserModel from "./oranizationUserModel";
-import { Request, Response } from "express";
 
-
-export const organizationRegister = async (req: Request, res: Response): Promise<Response> => {
-    const {name, contactEmail, email, password} = req.body;
-    const organizationExist = await oranizationUserModel.findOne({ email });
-    if (organizationExist) return res.status(400).json({ message: "Organization already exists!" });
-
-    const organization = new oranizationUserModel({ name, email, password, contactEmail });
-    await organization.save();
-    return res.json({ message: "Organization registration successful" });
-};
-
-    
