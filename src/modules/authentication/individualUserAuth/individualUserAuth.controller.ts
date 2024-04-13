7a362fd18ff0a2bc88a158206d5f827ce1cf6b00
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-04-13 09:29:31 +0100

    Add password confirmation check

--- src/modules/authentication/individualUserAuth/individualUserAuth.controller.ts
+++ src/modules/authentication/individualUserAuth/individualUserAuth.controller.ts
@@ -1,21 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import IndividualUser, { IndividaulDocument } from "./individualUserAuth.model";
+import IndividualUser, {
+  IndividualUserModel,
+} from "./individualUserAuth.model";
 import individualAuthPasswordToken from "./individualAuthPasswordToken";
 import Jwt from "jsonwebtoken";
 import { sendOtpEmail } from "../../../utils/email.utils";
 import catchAsync from "../../../utils/catchAsync";
 import AppError from "../../../utils/appError";
 
-export const generateToken = (length = 4) =>{
-  // decallar variable 
+export const generateToken = (length = 4) => {
+  // decallar variable
   let otp = "";
-  
-  for(let i = 0; i < length; i++){
-      const digit = Math.floor(Math.random() * 10)
-      otp += digit
+
+  for (let i = 0; i < length; i++) {
+    const digit = Math.floor(Math.random() * 10);
+    otp += digit;
   }
   return otp;
-}
+};
 
 export const individualUserRegistration = async (
   req: Request,
@@ -26,8 +28,8 @@ export const individualUserRegistration = async (
     const { email, phoneNumber, password, confirmPassword } = req.body;
 
     // Check if the user already exists
-    const userExists: IndividaulDocument | null = await IndividualUser.findOne(
-      {email}
+    const userExists: IndividualUserModel | null = await IndividualUser.findOne(
+      { email }
     );
 
     if (userExists) {
@@ -36,12 +38,18 @@ export const individualUserRegistration = async (
       });
     }
 
+    // Check if password and confirmPassword match
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        message: "Passwords do not match",
+      });
+    }
+
     // Create a new user
     const newUser = new IndividualUser({
       email,
       phoneNumber,
       password,
-      confirmPassword,
     });
 
     // Save the user to the database
@@ -57,7 +65,6 @@ export const individualUserRegistration = async (
   }
 };
 
-
 export const verifyIndividualUserEmail = async (
   req: Request,
   res: Response
@@ -68,16 +75,18 @@ export const verifyIndividualUserEmail = async (
     // Check if the user exists and is verified
     const user = await IndividualUser.findOne({ email });
     if (!user) {
-      return res.status(404).json({ message: "User not found or email does not match." });
+      return res
+        .status(404)
+        .json({ message: "User not found or email does not match." });
     }
     if (user.verified) {
       return res.status(400).json({ message: "User is already verified." });
     }
 
     // Perform email verification logic here...
-    
+
     // Update user's verification status
-    user.verified = true; 
+    user.verified = true;
     await user.save();
 
     // Respond with success message
@@ -87,7 +96,6 @@ export const verifyIndividualUserEmail = async (
     res.status(500).json({ message: "Internal server error." });
   }
 };
-
 
 // export const verifyIndividualUserEmail = async (
 //   req: Request,
@@ -186,19 +194,25 @@ export const verifyIndividualUserEmail = async (
 //   );
 //   const decoded = Jwt.decode(token);
 //   console.log(decoded);
-//   res.status(201).json({ token, userId: user._id }); 
+//   res.status(201).json({ token, userId: user._id });
 // };
 
 export const individualUserLogin = async (req: Request, res: Response) => {
-  const {email, password} = req.body;
-  const user = await IndividualUser.findOne({email});
-  if(!user) return res.status(400).json({message: "Email/Password mismatch!"});
+  const { email, password } = req.body;
+  const user = await IndividualUser.findOne({ email });
+  if (!user)
+    return res.status(400).json({ message: "Email/Password mismatch!" });
   const isMatch = await user.comparePassword(password);
-  if(!isMatch) return res.status(400).json({message: "Email/Password mismatch!"}); 
-  const token = Jwt.sign({userId: user._id}, process.env.JWT_SECRET || "secret", {expiresIn: "1h"});
+  if (!isMatch)
+    return res.status(400).json({ message: "Email/Password mismatch!" });
+  const token = Jwt.sign(
+    { userId: user._id },
+    process.env.JWT_SECRET || "secret",
+    { expiresIn: "1h" }
+  );
   // const decoded = Jwt.decode(token)
   // console.log(decoded);
-  res.status(201).json({token, userId: user._id});
+  res.status(201).json({ token, userId: user._id });
 };
 
 // export const generateOTP = async (req: Request, res: Response) => {
@@ -233,7 +247,7 @@ export const individualUserLogin = async (req: Request, res: Response) => {
 //     //   status: "success",
 //     //   message: "Token sent to email!",
 //     // });
-//     res.json({Token: newToken.token}); 
+//     res.json({Token: newToken.token});
 
 //   } catch (err) {
 //     // user. = undefined;
@@ -256,18 +270,18 @@ export const individualUserLogin = async (req: Request, res: Response) => {
 //       await user.save({ validateBeforeSave: false });
 
 //       console.log(token)
-    
+
 //       await individualAuthPasswordToken.findOneAndDelete({
 //         owner: userId
 //       });
-    
+
 //       const newToken = await individualAuthPasswordToken.create({
 //         owner: userId,
 //         token
 //       })
-    
+
 //       const message = `Forgot your password? Here is one time password for your reset password : ${token}.\nIf you didn't forget your password, please ignore this email!`;
-    
+
 //       try {
 //         // sendEmail function needs to be implemented separately
 //         await sendEmail({
@@ -275,13 +289,13 @@ export const individualUserLogin = async (req: Request, res: Response) => {
 //           subject: "Your password reset token (valid for 10 min)",
 //           message,
 //         });
-    
+
 //         // res.status(200).json({
 //         //   status: "success",
 //         //   message: "Token sent to email!",
 //         // });
-//         res.json({Token: newToken.token}); 
-    
+//         res.json({Token: newToken.token});
+
 //       } catch (err) {
 //         // user. = undefined;
 //         // user.passwordResetExpires = undefined;
@@ -296,65 +310,78 @@ export const individualUserLogin = async (req: Request, res: Response) => {
 //   }
 // );
 
-export const generateOTP = catchAsync(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    // 1) Get user based on POSTed email
-    const { email } = req.body;
-    if (!email) {
-      return next(new AppError("Please provide an email address.", 400));
+export const generateOTP = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      // 1) Get user based on POSTed email
+      const { email } = req.body;
+      if (!email) {
+        return next(new AppError("Please provide an email address.", 400));
+      }
+
+      const user = await IndividualUser.findOne({ email });
+      if (!user) {
+        return next(
+          new AppError("There is no user with that email address.", 404)
+        );
+      }
+
+      const userId = user._id;
+      const token = generateToken(); // Make sure generateToken function exists and generates secure tokens
+
+      await individualAuthPasswordToken.findOneAndDelete({ owner: userId });
+      const newToken = await individualAuthPasswordToken.create({
+        owner: userId,
+        token,
+      });
+
+      // const message = `Forgot your password? Here is a one-time password for resetting your password: ${token}.\nIf you didn't forget your password, please ignore this email.`;
+
+      // sendEmail function needs to be implemented separately
+      sendOtpEmail(token, email);
+
+      res.status(200).json({ token: newToken.token });
+    } catch (err) {
+      console.error("Error in generateOTP:", err);
+      return next(
+        new AppError(
+          "There was an error sending the email. Please try again later.",
+          500
+        )
+      );
     }
-    
-    const user = await IndividualUser.findOne({ email });
-    if (!user) {
-      return next(new AppError("There is no user with that email address.", 404));
-    }
- 
-    const userId = user._id;
-    const token = generateToken(); // Make sure generateToken function exists and generates secure tokens
-
-    await individualAuthPasswordToken.findOneAndDelete({ owner: userId });
-    const newToken = await individualAuthPasswordToken.create({ owner: userId, token });
-
-    // const message = `Forgot your password? Here is a one-time password for resetting your password: ${token}.\nIf you didn't forget your password, please ignore this email.`;
-
-    // sendEmail function needs to be implemented separately
-    sendOtpEmail(token, email) 
-
-    res.status(200).json({ token: newToken.token });
-  } catch (err) {
-    console.error("Error in generateOTP:", err);
-    return next(new AppError("There was an error sending the email. Please try again later.", 500));
   }
-});
-
+);
 
 export const verifyOTP = async (req: Request, res: Response) => {
-  const {email, token} = req.body;
+  const { email, token } = req.body;
   try {
-    const user = await IndividualUser.findOne({email})
-    if(!user) return res.status(400).json({message: "Invalid user!"})
-    const verifyToken = await individualAuthPasswordToken.findOne({owner: user._id});
+    const user = await IndividualUser.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid user!" });
+    const verifyToken = await individualAuthPasswordToken.findOne({
+      owner: user._id,
+    });
 
     if (!verifyToken) {
-        return res.status(403).json({ error: "Invalid token!"});
+      return res.status(403).json({ error: "Invalid token!" });
     }
 
     const matched = await verifyToken.compareToken(token);
 
     if (!matched) {
-        return res.status(403).json({ error: "Invalid token!" });
+      return res.status(403).json({ error: "Invalid token!" });
     }
 
     return res.json({ message: "Token verified!" });
-} catch (error) {
+  } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal Server Error" });
-}
+  }
 };
 
 export const resetIndividualPassword = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  
+
   try {
     // Find the user by email
     const user = await IndividualUser.findOne({ email });
