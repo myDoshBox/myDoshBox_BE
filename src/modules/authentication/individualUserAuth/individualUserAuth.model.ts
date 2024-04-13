f29247de94c16a8d32006752edf28abc0b7f716b
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-04-13 07:48:24 +0100

    reverted individual user auth model to previous version

--- src/modules/authentication/individualUserAuth/individualUserAuth.model.ts
+++ src/modules/authentication/individualUserAuth/individualUserAuth.model.ts
@@ -1,59 +1,114 @@
-import { compare, hash } from "bcrypt";
-import mongoose, { Model, model } from "mongoose";
+import { Document, model, Model, Schema } from "mongoose";
+import { emailValidator } from "../../../utils/validator.utils";
+import { hash, compare } from "bcrypt";
+import crypto from "crypto";
 
-// create a schema for the individual user
-
-export interface IndividaulDocument {
+// Interface representing the individual document
+export interface UserDocument extends Document {
   email: string;
   phoneNumber: string;
   password: string;
-  confirmPassword: string;
+  _confirmPassword?: string;
   verified: boolean;
+  passwordChangedAt?: Date;
+  passwordResetToken?: string;
+  passwordResetExpires?: Date;
 }
 
-
-interface Methods {
-  comparePassword(password: string): Promise<boolean>;
+// Helper functions for user management
+export interface UserModel extends Model<UserDocument> {
+  comparePassword(candidatePassword: string): Promise<boolean>;
+  createPasswordResetToken(): string;
 }
 
-const individualUserAuthSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const individualUserSchema: Schema<UserDocument> = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minlength: [5, "Email must be at least 5 characters"],
+      validate: {
+        validator: emailValidator,
+        message: "Invalid email format",
+      },
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+    verified: { type: Boolean, default: false },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
-  phoneNumber: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  confirmPassword: {
-    type: String,
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  }
-});
+  { timestamps: true }
+);
 
-individualUserAuthSchema.pre("save", async function (next) {
-  // Hash the token
+// Hash the password before saving it to the database
+individualUserSchema.pre<UserDocument>("save", async function (next) {
   if (this.isModified("password")) {
-      this.password = await hash(this.password, 10);
+    const saltRounds = 10;
+    this.password = await hash(this.password, saltRounds);
   }
   next();
 });
 
-individualUserAuthSchema.methods.comparePassword = async function (password: string) {
-  const result = await compare(password, this.password); // Change this.token to this.password
-  return result;
-}
+// Middleware to validate confirm password
+individualUserSchema.pre("validate", function (next) {
+  if (this.isModified("password") && this.password !== this._confirmPassword) {
+    this.invalidate("confirmPassword", "Passwords do not match");
+  }
+  next();
+});
 
+// Virtual field for confirm password (not stored in the database)
+individualUserSchema
+  .virtual("confirmPassword")
+  .get(function (this: { _confirmPassword: string }) {
+    return this._confirmPassword;
+  })
+  .set(function (this: { _confirmPassword: string }, value: string) {
+    this._confirmPassword = value;
+  });
 
-// create a model for the individual user
- 
-export default model("IndividualUser", individualUserAuthSchema) as Model<IndividaulDocument, {}, Methods>
+// Compare the password with the hashed password in the database
+individualUserSchema.methods.comparePassword = async function (
+  candidatePassword: string
+) {
+  return await compare(candidatePassword, this.password);
+};
+
+individualUserSchema.methods.createPasswordResetToken = function (
+  this: UserDocument
+) {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  const resetExpires = new Date();
+  resetExpires.setMinutes(resetExpires.getMinutes() + 10); // Add 10 minutes to the current time
+  this.passwordResetExpires = resetExpires;
+
+  return resetToken;
+};
+
+// Create a model for the individual user
+export default model("IndividualUser", individualUserSchema) as Model<
+  UserDocument,
+  object,
+  UserModel
+>;
