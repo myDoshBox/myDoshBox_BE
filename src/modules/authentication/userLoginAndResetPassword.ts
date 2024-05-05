b48fbb52955db66090159d4f2f71e7d8ad437413
Ajayi favour <ajayifavour81@gmail.com>
2024-05-05 19:51:25 +0100

    new update

--- src/modules/authentication/userLoginAndResetPassword.ts
+++ src/modules/authentication/userLoginAndResetPassword.ts
@@ -67,76 +67,79 @@ export const userLogin: RequestHandler = async (req: Request, res: Response) =>{
 
 
 
-// export const OrganizationUserForgotPassword = catchAsync(
-//     async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-//       // 1) Get user based on POSTed email
-//       const {email, organization_email} = req.body
-//       const org = await OrganizationModel.findOne({
-//         organization_email: req.body.email,
-//       });
-//       const user = await IndividualUser.findOne({
-//         email: req.body.email,
-//       });
-//       console.log(user)
-//       if (!org || !user) {
-//         return next(new AppError("There is no user with email address.", 404));
-//       }
-  
-//       // 2) Generate the random reset token
-//       const resetToken = org.createPasswordResetToken();
-//       await org.save({ validateBeforeSave: false });
-//       await user.save({ validateBeforeSave: false });
-  
-//       // 3) Send it to user's email
-//       const resetURL = `${req.protocol}://${req.get(
-//         "host"
-//       )}/auth/organization/resetPassword/${resetToken}`;
-  
-//       try {
-//         sendURLEmail([org.organization_email, user.email], resetURL);
-//         res.status(200).json({ message: "success" });
-//       } catch (err) {
-//         return next(new AppError("There is an error sending the email.", 500));
-//       }
-//     }
-//   );
-
 export const OrganizationUserForgotPassword = catchAsync(
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // 1) Get user based on POSTed email
-    const { email } = req.body;
-    const org = await OrganizationModel.findOne({
-      organization_email: email, // Use email directly, assuming it's the organization's email
-    });
-    const user = await IndividualUser.findOne({
-      email: email, // Use email directly
-    });
-    console.log(user);
-    if (!org && !user) { // Check if neither organization nor individual user found
-      return next(new AppError("There is no user with the provided email address.", 404));
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+      // 1) Get user based on POSTed email
+      const {email} = req.body
+      console.log(email)
+      const org = await OrganizationModel.findOne({
+        organization_email: email,
+      });
+      const user = await IndividualUser.findOne({
+        email,
+      });
+      console.log(user)
+      if (!org || !user) {
+        return next(new AppError("There is no user with email address.", 404));
+      }
+  
+      // 2) Generate the random reset token
+      // const resetToken = org.createPasswordResetToken();
+      // await org.save({ validateBeforeSave: false });
+      // await user.save({ validateBeforeSave: false });
+      const resetToken = org ? org.createPasswordResetToken() : user.createPasswordResetToken();
+      await (org ? org.save({ validateBeforeSave: false }) : user.save({ validateBeforeSave: false }));
+  
+      // 3) Send it to user's email
+      const resetURL = `${req.protocol}://${req.get(
+        "host"
+      )}/auth/organization/resetPassword/${resetToken}`;
+  
+      try {
+        sendURLEmail([org.organization_email, user.email], resetURL);
+        res.status(200).json({ message: "success" });
+      } catch (err) {
+        return next(new AppError("There is an error sending the email.", 500));
+      }
     }
+  );
 
-    // 2) Generate the random reset token
-    const resetToken = org ? org.createPasswordResetToken() : user.createPasswordResetToken();
+// export const OrganizationUserForgotPassword = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//     // 1) Get user based on POSTed email
+//     const { email } = req.body;
+//     const org = await OrganizationModel.findOne({
+//       organization_email: email, // Use email directly, assuming it's the organization's email
+//     });
+//     const user = await IndividualUser.findOne({
+//       email: email, // Use email directly
+//     });
+//     console.log(user);
+//     if (!org && !user) { // Check if neither organization nor individual user found
+//       return next(new AppError("There is no user with the provided email address.", 404));
+//     }
+
+//     // 2) Generate the random reset token
+//     const resetToken = org ? org.createPasswordResetToken() : user.createPasswordResetToken();
     
-    // Since we're not sure whether org or user is defined, check before saving
-    if (org) await org.save({ validateBeforeSave: false });
-    if (user) await user.save({ validateBeforeSave: false });
+//     // Since we're not sure whether org or user is defined, check before saving
+//     if (org) await org.save({ validateBeforeSave: false });
+//     if (user) await user.save({ validateBeforeSave: false });
 
-    // 3) Send it to user's email
-    const resetURL = `${req.protocol}://${req.get(
-      "host"
-    )}/auth/organization/resetPassword/${resetToken}`;
+//     // 3) Send it to user's email
+//     const resetURL = `${req.protocol}://${req.get(
+//       "host"
+//     )}/auth/organization/resetPassword/${resetToken}`;
 
-    try {
-         sendURLEmail([org.organization_email, user.email], resetURL);
-      // Filter out undefined emails
-      res.status(200).json({ message: "success" });
-    } catch (err) {
-      return next(new AppError("There is an error sending the email.", 500));
-    }
-  }
-);
+//     try {
+//          sendURLEmail([org.organization_email, user.email], resetURL);
+//       // Filter out undefined emails
+//       res.status(200).json({ message: "success" });
+//     } catch (err) {
+//       return next(new AppError("There is an error sending the email.", 500));
+//     }
+//   }
+// );
 
 
 //  export const organizationUserResetPassword = catchAsync(
