21b7268006a9586f526c31ea015b2f52c573fa9d
Pmanonline <petersonzoconis@gmail.com>
2025-10-30 12:04:08 +0100

    updated origin

--- src/config/allowedOrigins.config.ts
+++ src/config/allowedOrigins.config.ts
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  "http://localhost:3000/*",
-  "https://mydoshbox.vercel.app/*",
+  "http://localhost:3000",
+  "https://mydoshbox.vercel.app",
 ];
 
 export default allowedOrigins;
--- src/config/corsOptions.config.ts
+++ src/config/corsOptions.config.ts
@@ -1,16 +1,19 @@
 import allowedOrigins from "./allowedOrigins.config";
 
 const corsOptions = {
-    origin: (origin: string, callback: (arg0: Error | null, arg1: boolean | undefined) => void) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
-            callback(null, true);
-        } else{
-            callback(new Error("not allowed CORS"), false)
-        }
-    },
+  origin: (
+    origin: string,
+    callback: (arg0: Error | null, arg1: boolean | undefined) => void
+  ) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("not allowed CORS"), false);
+    }
+  },
 
-    credentials: true,
-    optionSuccessStatus: 200
-}
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 export default corsOptions;
