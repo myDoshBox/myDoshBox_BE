e2d42e40b6323b4b9f78d7deecf3904fa3108187
Pmanonline <petersonzoconis@gmail.com>
2025-11-19 23:01:50 +0100

    pushing updated code for database URl

--- src/config/dbconn.config.ts
+++ src/config/dbconn.config.ts
@@ -8,9 +8,7 @@ const connectDB = async () => {
 
   try {
     // await mongoose.connect(DATABASE_URI);
-    await mongoose.connect(
-      "mongodb+srv://umoru-admin:3Ode6lAAfIQh0MX7@umoru-dev.2zy4fyn.mongodb.net/MyDoshBox"
-    );
+    await mongoose.connect(DATABASE_URI);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     throw new Error(error);
