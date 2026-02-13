fcda9adc49c0a0b4f82fb2dc546eae1f54095bbf
Pmanonline <petersonzoconis@gmail.com>
2026-02-13 12:59:50 +0100

    added origin for admin

--- src/config/allowedOrigins.config.ts
+++ src/config/allowedOrigins.config.ts
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "https://mydoshbox.vercel.app",
   "http://localhost:3000",
   "http://localhost:5173",
+  "https://doshbox-administration-app.vercel.app",
 ].filter(Boolean);
 
 export default allowedOrigins;
